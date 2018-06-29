5b9330589fa4a99f9f3dc8729cec077cccf4a3f7
surender-karu <k_suren2001@yahoo.com>
2018-06-29 17:23:57 +0800

    Mock exam page fix

--- src/client/containers/MockExams.jsx
+++ src/client/containers/MockExams.jsx
@@ -1,16 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { ListGroup, ListGroupItem, Jumbotron } from 'reactstrap'
+import _ from 'lodash';
+
+import axios from 'axios'
 
 class MockExams extends Component {
+    state = {
+        h3: null,
+        content: null,
+        links: []
+    };
+
+    componentDidMount() {
+        axios.get('/api/mock_exam')
+            .then(response => {
+                const data = response.data;
+
+                this.setState(data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
     render() {
         return (
             <div className="container">
-                <h3>
-                    11 Plus Mock Exams
-
-                </h3>
-                <p>Our Mock exam papers are based on last year’s grammar exam paper, taken from asking students who have
-                    successfully undertaken last year’s 11+ exams for following schools:
-                </p>
+                <Jumbotron className="transparent-bg">
+                    <h3 className="display-4">
+                        {this.state.h3}
+                    </h3>
+                    <p className="lead">{this.state.content}
+                    </p>
+                </Jumbotron>
+                <ListGroup flush>
+                    {
+                        _.map(this.state.links, (link, i) => {
+                            return (<ListGroupItem key={i.toString()} tag="a" href={link.href}>{link.name}</ListGroupItem>)
+                        }
+                        )
+                    }
+                </ListGroup>
             </div>
         );
     }
